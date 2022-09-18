e6b1edacaa143a144b78691d9e3ec92748a7d899
AnsonInTheAstroworld <haoyunhhy@foxmail.com>
2022-09-18 19:49:14 +0800

    9.18 19:49

--- functions.cpp
+++ functions.cpp
@@ -3,6 +3,11 @@
 #include<sys/stat.h>
 #include<fcntl.h>
 #include<unistd.h>
+#include<dirent.h>
+#include<iostream>
+#include<string.h>
+using namespace std;
+
 bool ModifyContent(const char* fileName, int offset, const char* buf)
 {
 	int fd =-1;
@@ -23,8 +28,69 @@ bool ModifyContent(const char* fileName, int offset, const char* buf)
 	else return false;
 }
 
-bool ChangeDirectory()
+bool ChangeDirectory(const char* pathname)
 {
+	// 成功返回0，失败返回-1
+	if (!chdir(pathname)) {
+		cout << "当前目录：" << getcwd(NULL, 0) << endl;
+		return true;
+	}
+	else {
+		return false;
+	}
+}
 
-	return false;
+bool ChangeDirectory(int fd)
+{
+	// 成功返回0，失败返回-1
+	if (!fchdir(fd)) {
+		cout << "当前目录：" << getcwd(NULL, 0) << endl;
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+bool CheckIsFileOrDir(const char* name)
+{
+	struct stat st;
+
+	// 成功返回0，失败返回-1
+	if (!stat(name, &st)) {
+		if (S_ISDIR(st.st_mode)) {
+			DIR* dir;
+			struct dirent* file;
+
+			if (!(dir = opendir(name))) return false;
+
+			while ((file = readdir(dir)) != NULL) {
+				if (strncmp(file->d_name, ".", 1) == 0)
+					continue;
+				if(!stat(file->d_name, &st))
+					if (!S_ISDIR(st.st_mode))
+						cout << file->d_name << endl;
+			}
+
+			return true;
+		}
+		else {
+			if (st.st_mode & S_IROTH)
+			{
+				cout << "该文件只读" << endl;
+			}
+			if (st.st_mode & S_IWOTH)
+			{
+				cout << "该文件只写" << endl;
+			}
+			if (st.st_mode & S_IXOTH)
+			{
+				cout << "该文件读写" << endl;
+			}
+			return true;
+		}
+	}
+	else {
+		return false;
+	}
 }
--- functions.h
+++ functions.h
@@ -1,4 +1,8 @@
 #pragma once
 bool ModifyContent(const char* fileName, int offset, const char* buf);
 
-bool ChangeDirectory();
+bool ChangeDirectory(const char* pathname);
+
+bool ChangeDirectory(int fd);
+
+bool CheckIsFileOrDir(const char* name);
